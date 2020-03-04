292a6f66ad29b55261f2cd85f2aee63ea89e44ed
prostoqa <elalbondareva@gmail.com>
2020-03-05 01:40:06 +0300

    one task from previous module

---
+++ section4_lesson2_step7.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time, math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Выполнение всех условий задачи
+    image = browser.find_element_by_tag_name("img")
+    x = image.get_attribute("valuex")
+    y = calc(x)
+    input = browser.find_element_by_id("answer")
+    input.send_keys(y)
+    checkbox = browser.find_element_by_id("robotCheckbox")
+    checkbox.click()
+    radiobutton = browser.find_element_by_id("robotsRule")
+    radiobutton.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
